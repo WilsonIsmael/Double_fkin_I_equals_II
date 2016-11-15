54aab4a7223f2ffdb5a8c38d701576406f8d42b4
Utilizador <Utilizador@DESKTOP-03T212H>
2016-11-15 19:34:07 +0000

    wilson

---
+++ mes/src/mes/Protocol.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mes;
+
+import java.io.*;
+import java.net.*;
+
+class Protocol
+{
+   public static void UDPServer(String args[]) throws Exception
+      {
+         DatagramSocket serverSocket = new DatagramSocket(54321);
+            byte[] receiveData = new byte[1024];
+            byte[] sendData = new byte[1024];
+            while(true)
+               {
+                  DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                  serverSocket.receive(receivePacket);
+                  String sentence = new String( receivePacket.getData());
+                  System.out.println("RECEIVED: " + sentence);
+                  InetAddress IPAddress = receivePacket.getAddress();
+                  int port = receivePacket.getPort();
+                  String capitalizedSentence = sentence.toUpperCase();
+                  sendData = capitalizedSentence.getBytes();
+                  DatagramPacket sendPacket =
+                  new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                  serverSocket.send(sendPacket);
+               }
+      }
+}

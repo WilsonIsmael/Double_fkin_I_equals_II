ef02fa77c2c69f6deb073c07f19a5919ceee9bea
ee10092 <ee10092@B02AU1048.fe.up.pt>
2016-11-25 11:21:28 +0000

    Teste em manager·

--- mes/src/mes/Manager.java
+++ mes/src/mes/Manager.java
@@ -3,12 +3,21 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package mes;
 
+import java.util.Scanner;
 /**
  *
  * @author Utilizador
  */
 public class Manager {
     
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Insert int: ");
+        int n = input.nextInt();
+        System.out.println("O utilizador inseriu: " + n);
+    }
 }
